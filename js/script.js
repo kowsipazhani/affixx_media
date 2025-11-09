c4115f391d00494549f26f51185af5fc31e8ea9a
kowsipazhani <kowsi02061997@gmail.com>
2025-11-09 21:52:30 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ Promise.all([
     fetch("header.html").then(res => res.text()),
     fetch("footer.html").then(res => res.text()),
     fetch("sidebar.html").then(res => res.text()),
-    fetch("search-form.html").then(res => res.text())
+    //fetch("search-form.html").then(res => res.text())
   ])
   .then(([headerHTML, footerHTML, sidebarHTML, searchHTML]) => {
     $("#header").html(headerHTML);
     $("#footer").html(footerHTML);
     $("#sidebar").html(sidebarHTML);
     $("#edit-sidebar").html(sidebarHTML);
-    $("#search-form-container").html(searchHTML);
+    //$("#search-form-container").html(searchHTML);
   })
   .then(() => {
     initBannerVideo();
@@ -19,7 +19,7 @@ Promise.all([
     initSidebarDropdown();
     initCounter();
     initThemeSwitch();
-    initSearchBar();
+    //initSearchBar();
     initSubmitContact();
     initSubmitNewsletter();
     initAnimateData();
